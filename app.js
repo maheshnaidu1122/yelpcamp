c5db160a2780110b30fa650a4fe14a4545774c4f
maheshnaidu1122 <akulam308@gmail.com>
2025-02-07 20:33:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const Sanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo');
 // MongoDB connection URL
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';  // Use 127.0.0.1 (IPv4 address)
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';  // Use 127.0.0.1 (IPv4 address)
 
 // Connect to MongoDB
 // Connect to MongoDB
