c4bc56b4664849516e3f120e4d8c7530e6aa9f8a
maheshnaidu1122 <akulam308@gmail.com>
2025-02-07 21:35:23 +0530

    Delete error directory

--- error/CatchAsync.js
+++
@@ -1,6 +0,0 @@
-module.exports = fun => {
-    return (req, res, next) => {
-        // Run the function and catch any errors to pass to the next middleware
-        fun(req, res, next).catch(next);
-    }
-}
--- error/Expresserror.js
+++
@@ -1,8 +0,0 @@
-class Expresserror extends Error{
-    constructor(message,statusCode){
-        super();
-        this.message=message;
-        this.statusCode=statusCode;
-    }
-}
-module.exports=Expresserror
